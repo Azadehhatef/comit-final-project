b030c16e8d6f7ac757600cbe8de9259c3cf557cd
Azadeh Hatef <hatef.azadeh@gmail.com>
2019-12-19 17:33:31 -0600

    Updates

---
+++ precache-manifest.e050fec887cb5a51d2c0c2b7f2870d9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2c54a71c6dd4260118442f9f259e6d",
+    "url": "/comit-final-project/index.html"
+  },
+  {
+    "revision": "40bfb5e4a89fb1554860",
+    "url": "/comit-final-project/static/css/main.811b7885.chunk.css"
+  },
+  {
+    "revision": "64cf7a40bf258289c100",
+    "url": "/comit-final-project/static/js/2.b6226cb9.chunk.js"
+  },
+  {
+    "revision": "40bfb5e4a89fb1554860",
+    "url": "/comit-final-project/static/js/main.2c93e7e3.chunk.js"
+  },
+  {
+    "revision": "5a04b1db535aac1da1c4",
+    "url": "/comit-final-project/static/js/runtime-main.7b1809e5.js"
+  },
+  {
+    "revision": "da722b5659fcd8fa1bc17a983687e7e8",
+    "url": "/comit-final-project/static/media/cover.da722b56.png"
+  },
+  {
+    "revision": "72781ead282c4267cb3d69c314e50a2a",
+    "url": "/comit-final-project/static/media/ecotoxicolocy.72781ead.jpg"
+  },
+  {
+    "revision": "8cf8bcd2861d43fd956911eb1f28a272",
+    "url": "/comit-final-project/static/media/fish.8cf8bcd2.png"
+  },
+  {
+    "revision": "82e06a4cf0f6e11ff920cd6fa9765eec",
+    "url": "/comit-final-project/static/media/metabolism.82e06a4c.jpg"
+  }
+]);
